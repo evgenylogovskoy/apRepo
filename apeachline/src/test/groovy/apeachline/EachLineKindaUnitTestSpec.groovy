af89486e08d2c7f27eca13bbde477896aef8a69a
evgeny <evgenyl@jfrog.com>
2019-02-08 17:16:22 +0200

    asd

--- apeachline/src/test/groovy/apeachline/EachLineKindaUnitTestSpec.groovy
+++ apeachline/src/test/groovy/apeachline/EachLineKindaUnitTestSpec.groovy
@@ -10,7 +10,7 @@ import spock.lang.*
 
 class EachLineKindaUnitTestSpec extends Specification {
 
-
+	@TestRail(testCaseId =  "108343")
 	def "Verify if eachLine method iterates through each line"(){
 		
 		given:
@@ -24,7 +24,7 @@ class EachLineKindaUnitTestSpec extends Specification {
 		then: "Verify each element of the actualResultsList"
 			actualResultsList == ["", "line2", "line3"]
 	}
-	@TestRail(testCaseId =  "108343")
+	@TestRail(testCaseId =  "108344")
 	def "Check if it returns the last value returned by closure"(){
 		
 		given:
@@ -36,7 +36,7 @@ class EachLineKindaUnitTestSpec extends Specification {
 		then: "Verify the last value returned by the closure"
 			result == "and this is a last line"
 	}
-	@TestRail(testCaseId =  "33333")
+	@TestRail(testCaseId =  "108345")
 	def "Test firstLine parameter default"(){
 		
 		given:
@@ -58,7 +58,7 @@ class EachLineKindaUnitTestSpec extends Specification {
 		then: "Verify first line "
 			actualString == "This is simple multiline string,"
 	}
-
+	@TestRail(testCaseId =  "108346")
 	def "Test firstLine parameter custom"(){
 		
 		given:
