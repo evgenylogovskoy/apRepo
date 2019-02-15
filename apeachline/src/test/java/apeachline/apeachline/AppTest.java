9c01d309341d6a43118e1810fd428c71c2bed7b8
evgeny <evgenyl@jfrog.com>
2019-02-15 10:58:12 +0200

    asd

---
+++ apeachline/src/test/java/apeachline/apeachline/AppTest.java
@@ -0,0 +1,15 @@
+package apeachline.apeachline;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
